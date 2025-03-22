57a95e4a41fd530ab1024a2ec58ea67af2b0596a
Ngetich-86 <gideonkngetich86@gmail.com>
2025-03-22 13:02:51 +0300

    restaraunt app

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const items = document.querySelectorAll(".fade-in");
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.style.opacity = "1";
+                entry.target.style.transform = "translateY(0)";
+            }
+        });
+    });
+
+    items.forEach(item => observer.observe(item));
+});
